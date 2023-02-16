18c5a94e4cd8da4ccd38ca1361733f8a4a26d3a9
dedalovitor <dedalovitor@gmail.com>
2023-02-16 00:29:56 +0000

    casi tengo lo del perfil de usuario comarca

--- src/api/routes.py
+++ src/api/routes.py
@@ -42,12 +42,6 @@ def current_user_email():
     user = User.query.get(user_id)
     return jsonify({"response": "Hola", "email": user.email}), 200
 
-@api.route('/regions', methods=['GET'])
-def get_regions():
-    regions = Region.query.all()
-    return jsonify({"result": [x.serialize()for x in regions]}), 200
-
-
 
 @api.route('/register', methods=['POST'])
 def getting_register():
@@ -113,3 +107,36 @@ def user_region_login():
      token = create_access_token(identity=user.id)
      
      return jsonify({"response": "hola", "token": token}), 200
+
+@api.route('/region', methods=['POST'])
+@jwt_required()
+def create_region():
+    user_id = get_jwt_identity()
+    body_name = request.json.get("name")
+    body_resume = request.json.get("resume")
+    body_photo = request.json.get("photo")
+    body_logo = request.json.get("logo")
+  
+    new_region = Region(name=body_name, resume=body_resume, photo=body_photo, logo=body_logo, user_id=user_id )
+    db.session.add(new_region)
+    db.session.commit()
+    return jsonify({"response": "Region registered successfully"}), 200
+
+@api.route('/regions', methods=['GET'])
+@jwt_required()
+def get_all_current_user_regions():
+    user_id = get_jwt_identity()
+    regions = Region.query.filter_by(user_id= user_id)
+    return jsonify({"result": [x.serialize() for x in regions]}), 200
+
+
+@api.route('/region/<int:region_id>', methods=['DELETE'])
+@jwt_required()
+def delete_region(region_id):
+    user_id = get_jwt_identity()
+    region = Region.query.get(region_id)
+    if region.user_id == user_id: 
+     db.session.delete(region)
+     db.session.commit()
+     return jsonify({ "response": "Region deleted correctly"}), 200
+     return jsonify({ "response": "Region not deleted"}), 400
