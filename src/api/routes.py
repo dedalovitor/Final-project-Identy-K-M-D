3fa39a303db8bb5d0766bb648785bd60287d3442
dedalovitor <dedalovitor@gmail.com>
2023-02-22 20:37:01 +0000

    nuevos cambios en profile-user-region

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Region, Restoration, User_region
+from api.models import db, User, Region, Restoration, User_region, Patrimony
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
@@ -164,4 +164,71 @@ def delete_region(region_id):
      db.session.commit()
      return jsonify({ "response": "Region deleted correctly"}), 200
      return jsonify({ "response": "Region not deleted"}), 400
-    
+
+
+@api.route('/patrimony', methods=['POST'])
+@jwt_required()
+def create_patrimony():
+    region_id = get_jwt_identity()
+    body_name = request.json.get("name")
+    body_resume = request.json.get("resume")
+    body_photo = request.json.get("photo")
+    body_logo = request.json.get("logo")
+  
+    new_patrimony = Patrimony(name=body_name, resume=body_resume, photo=body_photo, logo=body_logo, region_id=region_id)
+    db.session.add(new_patrimony)
+    db.session.commit()
+    return jsonify({"response": "Patrimony registered successfully"}), 200
+
+@api.route('/patrimonys', methods=['GET'])
+@jwt_required()
+def get_current_region_patrimonys():
+    region_id = get_jwt_identity()
+    patrimonys = Patrimony.query.filter_by(region_id= region_id)
+    return jsonify({ "results": [x.serialize() for x in patrimonys ]}), 200
+
+@api.route('/patrimony/<int:region_id>', methods=['DELETE'])
+@jwt_required()
+def delete_patrimony(region_id):
+    user_id = get_jwt_identity()
+    patrimony = Patrimony.query.get(region_id)
+    if user_id == user_id: 
+     db.session.delete(patrimony)
+     db.session.commit()
+     return jsonify({ "response": "Patrimony deleted correctly"}), 200
+     return jsonify({ "response": "Patrimony not deleted"}), 400
+
+
+@api.route('/restoration', methods=['POST'])
+@jwt_required()
+def create_restoration():
+    region_id = get_jwt_identity()
+    body_name = request.json.get("name")
+    body_resume = request.json.get("resume")
+    body_photo = request.json.get("photo")
+    body_logo = request.json.get("logo")
+    body_type_bussiness = request.json.get("type_bussiness")
+  
+    new_restoration = Restoration(name=body_name, resume=body_resume, photo=body_photo, logo=body_logo, type_bussiness=body_type_bussiness, region_id=region_id)
+    db.session.add(new_restoration)
+    db.session.commit()
+    return jsonify({"response": "Restoration registered successfully"}), 200
+
+
+@api.route('/restorations', methods=['GET'])
+@jwt_required()
+def get_current_region_restorations():
+    region_id = get_jwt_identity()
+    restorations = Restoration.query.filter_by(region_id= region_id)
+    return jsonify({ "results": [x.serialize() for x in restorations ]}), 200
+
+@api.route('/restoration/<int:region_id>', methods=['DELETE'])
+@jwt_required()
+def delete_restoration(region_id):
+    user_id = get_jwt_identity()
+    restoration = Restoration.query.get(region_id)
+    if user_id == user_id: 
+     db.session.delete(patrimony)
+     db.session.commit()
+     return jsonify({ "response": "Patrimony deleted correctly"}), 200
+     return jsonify({ "response": "Patrimony not deleted"}), 400
--- src/front/js/pages/profileuserregion.js
+++ src/front/js/pages/profileuserregion.js
@@ -7,9 +7,22 @@ export const Profileuserregion = () => {
     const { store, actions } = useContext(Context);
     const [region, setRegion] = useState({ name: "", resume: "", photo: "", logo: "" });
     const [regions, setRegions] = useState([]);
+    const [patrimony, setPatrimony] = useState({ name: "", resume: "", photo: "", logo: "" });
+    const [patrimonys, setPatrimonys] = useState([]);
+    const [restoration, setRestoration] = useState({ name: "", resume: "", photo: "", logo: "" });
+    const [restorations, setRestorations] = useState([]);
+
 
     useEffect(() => {
         getCurrentUserRegions();
+    }, [])
+
+    useEffect(() => {
+        getCurrentRegionPatrimony();
+    }, [])
+
+    useEffect(() => {
+        getCurrentRegionRestoration();
     }, [])
 
     const getCurrentUserRegions = async () => {
@@ -22,6 +35,30 @@ export const Profileuserregion = () => {
         });
         const data = await response.json();
         setRegions(data.results);
+    }
+
+    const getCurrentRegionPatrimony = async () => {
+        const response = await fetch(process.env.BACKEND_URL + "/api/patrimonys", {
+            method: "GET",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        const data = await response.json();
+        setPatrimonys(data.results);
+    }
+
+    const getCurrentRegionRestoration = async () => {
+        const response = await fetch(process.env.BACKEND_URL + "/api/restorations", {
+            method: "GET",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        const data = await response.json();
+        setRestorations(data.results);
     }
 
     const createRegion = async () => {
@@ -47,6 +84,52 @@ export const Profileuserregion = () => {
         if (response.ok) getCurrentUserRegions();
     }
 
+
+    const createPatrimony = async () => {
+        const response = await fetch(process.env.BACKEND_URL + "/api/patrimony", {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+            body: JSON.stringify(patrimony)
+        });
+        if (response.ok) getCurrentUserRegions();
+    }
+
+    const deletePatrimony = async (id) => {
+        const response = await fetch(process.env.BACKEND_URL + "/api/patrimony/" + id, {
+            method: "DELETE",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        if (response.ok) getCurrentRegionPatrimony();
+    }
+
+    const createRestoration = async () => {
+        const response = await fetch(process.env.BACKEND_URL + "/api/restoration", {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+            body: JSON.stringify(restoration)
+        });
+        if (response.ok) getCurrentRegionRestoration();
+    }
+
+    const deleteRestoration = async (id) => {
+        const response = await fetch(process.env.BACKEND_URL + "/api/restoration/" + id, {
+            method: "DELETE",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        });
+        if (response.ok) getCurrentRegionRestoration();
+    }
 
     return (
         <>
@@ -78,6 +161,71 @@ export const Profileuserregion = () => {
                         </div>
                         <div className="card-footer">
                             <button className="btn btn-danger" onClick={() => deleteRegion(x.id)}>DEL</button>
+                        </div>
+                    </div>
+                })}
+
+            </div>
+            <div className="card COL-5 w-25 p-3">
+                {Object.keys(patrimony).map((key, i) => {
+                    if (typeof patrimony[key] != "boolean") {
+                        return <input placeholder={key} key={i} name={key} defaultValue={patrimony[key]}
+                            onChange={(e) => setPatrimony({ ...patrimony, [key]: e.target.value })}>
+                        </input>
+                    } else {
+                        return <input type="checkbox" key={i} name={key} checked={patrimony[key]}
+                            onChange={(e) => setPatrimony({ ...patrimony, [key]: e.target.checked })}>
+                        </input>
+                    }
+
+                })}
+                <button className="btn btn-success" onClick={() => createPatrimony()}>CREATE PATRIMONY</button>
+            </div>
+            <div className="row">
+                {patrimonys.map((x) => {
+                    return <div key={x.id} className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src="https://www.cdc.gov/healthypets/images/pets/angry-dog.jpg?_=03873" alt="Card image cap" />
+                        <div className="card-body">
+                            <p className="card-text"> name: {x.name} </p>
+                            <p className="card-text"> resume: {x.resume} </p>
+                            <p className="card-text"> photo: {x.photo} </p>
+                            <p className="card-text"> logo: {x.logo} </p>
+
+                        </div>
+                        <div className="card-footer">
+                            <button className="btn btn-danger" onClick={() => deletePatrimony(x.id)}>DEL</button>
+                        </div>
+                    </div>
+                })}
+
+            </div>
+            <div className="card COL-5 w-25 p-3">
+                {Object.keys(restoration).map((key, i) => {
+                    if (typeof restoration[key] != "boolean") {
+                        return <input placeholder={key} key={i} name={key} defaultValue={restoration[key]}
+                            onChange={(e) => setRestoration({ ...restoration, [key]: e.target.value })}>
+                        </input>
+                    } else {
+                        return <input type="checkbox" key={i} name={key} checked={restoration[key]}
+                            onChange={(e) => setRestoration({ ...patrimony, [key]: e.target.checked })}>
+                        </input>
+                    }
+
+                })}
+                <button className="btn btn-success" onClick={() => createRestoration()}>CREATE RESTORATION</button>
+            </div>
+            <div className="row">
+                {restorations.map((x) => {
+                    return <div key={x.id} className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src="https://www.cdc.gov/healthypets/images/pets/angry-dog.jpg?_=03873" alt="Card image cap" />
+                        <div className="card-body">
+                            <p className="card-text"> name: {x.name} </p>
+                            <p className="card-text"> resume: {x.resume} </p>
+                            <p className="card-text"> photo: {x.photo} </p>
+                            <p className="card-text"> logo: {x.logo} </p>
+                        </div>
+                        <div className="card-footer">
+                            <button className="btn btn-danger" onClick={() => deleteRestoration(x.id)}>DEL</button>
                         </div>
                     </div>
                 })}
