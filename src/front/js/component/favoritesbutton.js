ed97b95891428a04d33fa9daff114548e280cb73
KARENNGB <karengutierrezb@hotmail.com>
2023-03-05 21:23:13 +0000

    creating new component

---
+++ src/front/js/component/favoritesbutton.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+{
+  store.userInfo ? (
+    <button type="button" class="btn btn-outline-success">
+      Success
+      <Favorites />
+    </button>
+  ) : (
+    ""
+  );
+}
