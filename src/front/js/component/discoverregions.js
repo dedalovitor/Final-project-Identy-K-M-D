b7d2946e419f0c285e236d15858758e6856d6899
dedalovitor <dedalovitor@gmail.com>
2023-03-19 20:31:13 +0000

    last changes

--- src/front/js/component/discoverregions.js
+++ src/front/js/component/discoverregions.js
@@ -6,16 +6,17 @@ export const Discover = () => (
     <div class="col-md-8 mx-auto">
       <div className="input-group col-12 rounded p-4 custom-discover align-items-center">
         <div>
-          <h2 style={{ marginBlockStart: "20rem", color: "white" }}>
+          <h2 className="titulo" style={{ marginBlockStart: "15rem", color: "white" }}>
             <strong>Regiones que no te puedes perder</strong>
           </h2>
-          <h5 style={{ color: "white" }}>
+          <h5 className="titulo" style={{ color: "white" }}>
             Tu guía de los mejores destinos, alojamientos y experiencias de esta temporada.
           </h5>
 
+
           <Link to="/discoregion">
-            <button type="button" className="btn btn-danger">
-              Explora Ahora
+            <button type="button" className="btn btn-danger mt-3">
+              <h5>Explora Ahora</h5>
             </button>
           </Link>
         </div>
